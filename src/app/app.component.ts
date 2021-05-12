7d6b2a3b911cb804661f4a1fb84fbdba1fff0040
sam <sblan45@gmail.com>
2021-05-12 14:55:49 -0400

    adds wikipedia service logic and ability to make http requests to wikipedia api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {WikipediaService} from './wikipedia.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wikisearch';
+  pages = []
+  constructor(private wikipedia: WikipediaService) {
+
+  }
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
+      console.log(response.query.search)
+    })
+  }
 }
